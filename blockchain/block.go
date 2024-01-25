753c19491b78347708cd94bfb2f774284348810c
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-01-25 14:56:54 -0300

    Add ComputeHash method to Block struct

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,7 +1,20 @@
 package blockchain
 
+import (
+	"bytes"
+	"crypto/md5"
+)
+
 type Block struct {
 	Hash     string
 	Data     string
 	PrevHash string
+}
+
+func (b *Block) ComputeHash() {
+	concatenatedData := bytes.Join([][]byte{[]byte(b.Data), []byte(b.PrevHash)}, []byte{})
+
+	computedHash := md5.Sum(concatenatedData)
+
+	b.Hash = string(computedHash[:])
 }
