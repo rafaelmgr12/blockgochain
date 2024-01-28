b75ea304dfc046f17077425482a3b14349489d00
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-01-28 14:03:17 -0300

    Refactor proof of work algorithm in blockchain package

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,6 +1,12 @@
 package blockchain
 
-import "math/big"
+import (
+	"bytes"
+	"crypto/md5"
+	"encoding/binary"
+	"fmt"
+	"math/big"
+)
 
 const Difficulty = 10
 
@@ -13,4 +19,45 @@ func NewProofOfWork(b *Block) *ProofOfWork {
 	target := big.NewInt(1)
 	target.Lsh(target, uint(256-Difficulty))
 	return &ProofOfWork{b, target}
+}
+
+func (pow *ProofOfWork) ComputeData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			[]byte(pow.Block.PrevHash),
+			[]byte(pow.Block.Data),
+			make([]byte, 8),
+			make([]byte, 8),
+		},
+		[]byte{},
+	)
+	binary.BigEndian.PutUint64(data[len(data)-16:], uint64(nonce))
+	binary.BigEndian.PutUint64(data[len(data)-8:], uint64(Difficulty))
+
+	return data
+}
+
+func (pow *ProofOfWork) MineBlock() (int, []byte) {
+	var intHash big.Int
+	var computedHash [16]byte
+
+	nonce := 0
+
+	for {
+		computedData := pow.ComputeData(nonce)
+		computedHash = md5.Sum(computedData)
+
+		fmt.Printf("\r%x", computedHash)
+
+		intHash.SetBytes(computedHash[:])
+
+		if intHash.Cmp(pow.Target) == -1 {
+			break
+		}
+
+		nonce++
+	}
+	fmt.Println()
+
+	return nonce, computedHash[:]
 }
