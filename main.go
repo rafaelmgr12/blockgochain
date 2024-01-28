35936db29867b502c0823324cdce6b417d6b98de
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-01-28 10:55:11 -0300

    Add main.go file with blockchain implementation

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rafaelmgr12/blockgochain/blockchain"
+)
+
+func main() {
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("Block 1")
+	chain.AddBlock("Block 2")
+	chain.AddBlock("Block 3")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Previous hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash of block: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
